ad093b256a2923af244ec79d2be120515aca8df3
Clemen <clemente.quintana@ntasys.com>
2022-10-24 08:18:36 +0200

    Create config-srv

---
+++ config-srv/src/main/java/com/config/srv/ConfigSrvApplication.java
@@ -0,0 +1,13 @@
+package com.config.srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSrvApplication.class, args);
+	}
+
+}
---
+++ config-srv/src/test/java/com/config/srv/ConfigSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
